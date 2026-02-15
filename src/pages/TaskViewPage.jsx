53a6e479c25cefb8a77f4cfe9e4ba8cfcbb5ab35
Pascal4195 <Ndukwepraise07@gmail.com>
2026-02-15 11:47:52 +0100

    Update TaskViewPage.jsx

--- src/pages/TaskViewPage.jsx
+++ src/pages/TaskViewPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from './Navbar';
 import { useTaskManager } from '../hooks/useTaskManager';
 import { useWallet } from '../context/WalletContext';
 
